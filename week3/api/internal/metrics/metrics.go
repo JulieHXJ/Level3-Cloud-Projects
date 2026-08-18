aa5e3d4e2a8b88945e9d90c5aceb931a6d7ac843
huangxj1993@gmail.com <XiaojunHuang@1b3.cluster>
2026-08-18 17:42:23 +0200

    add metrics request duration

--- week3/api/internal/metrics/metrics.go
+++ week3/api/internal/metrics/metrics.go
@@ -3,6 +3,7 @@ package metrics
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -12,10 +13,25 @@ import (
 var HTTPRequestsTotal = promauto.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: "cloud3_http_requests_total",
-		Help: "Total number of HTTP requests handled by the Cloud3 API.",
+		Help: "Total number of HTTP requests handled by API.",
 	},
 	[]string{"method", "status"},
 )
+
+
+var HTTPRequestsDuration = promauto.NewHistogramVec(
+	prometheus.HistogramOpts{
+		Name: "cloud3_http_request_duration_seconds",
+		Help: "Duration of HTTP requests handled by API",
+	},
+	[]string{"method"},
+	// add request_id, instance_id later
+)
+
+
+
+
+
 
 type statusRecorder struct {
 	http.ResponseWriter
@@ -30,6 +46,10 @@ func (w *statusRecorder) WriteHeader(status int) {
 // record http request and response status, count +1
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		start := time.Now()
+
+
 		// Prometheus scraping /metrics doesn't count as an API request.
 		// if r.URL.Path == "/metrics" {
 		// 	next.ServeHTTP(w, r)
@@ -45,12 +65,22 @@ func Middleware(next http.Handler) http.Handler {
 		// let response go through recorder, so we can have status code
 		next.ServeHTTP(recorder, r)
 
+
+
+
+		duration := time.Since(start).Seconds()
+
+
+
 		// request total +1
-		HTTPRequestsTotal.
-			WithLabelValues(
-				r.Method,
-				strconv.Itoa(recorder.statusCode),
-			).Inc()
+		HTTPRequestsTotal.WithLabelValues(
+			r.Method,
+			strconv.Itoa(recorder.statusCode),
+		).Inc()
+
+		HTTPRequestsDuration.WithLabelValues(r.Method).Observe(duration)
+
+
 	})
 }
 
